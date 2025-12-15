953364ac379783f05e70f91f5adb941c2684778b
bsnieg1 <bartoszsnieg@gmail.com>
2025-12-15 23:29:33 +0100

    main in loop

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ import utils.camera
 import threading
 import time
 
-print("this is main")
+while True:
+    print("this is main")
 
-print("1 - Dodaj użytkownika")
-print("2 - Skanuj QR")
+    print("1 - Dodaj użytkownika")
+    print("2 - Skanuj QR")
+    print("3 - Wyjście")
+    choice = input("Wybierz opcję: ")
 
-choice = input("Wybierz opcję: ")
+    if choice == "1":
+        name = input("Podaj imię: ")
+        user_id = add_user(name)
 
-if choice == "1":
-    name = input("Podaj imię: ")
-    user_id = add_user(name)
+        generate_qr(user_id, f"user_{user_id}.png")
+        print(f"Użytkownik {name} dodany z ID {user_id}")
 
-    generate_qr(user_id, f"user_{user_id}.png")
-    print(f"Użytkownik {name} dodany z ID {user_id}")
+    elif choice == "2":
 
-elif choice == "2":
+        t = threading.Thread(target=utils.camera.camera)
+        t.start()
+        print("Main Thread is running")
 
-    t = threading.Thread(target=utils.camera.camera)
-    t.start()
-    print("Main Thread is running")
-    time.sleep(20)
-    print("Stopping Camera")
-    utils.camera.z = False
+    elif choice == "3":
+        break
+
 
 
 
